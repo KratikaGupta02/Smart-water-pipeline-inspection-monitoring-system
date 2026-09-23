e5ddc2851ffe0e2ab6f530a80fa368b141e53243
KratikaGupta02 <kratikaaaa2006@gmail.com>
2026-09-23 23:12:01 +0530

    Initialize frontend structure and basic layout Co-authored-by: Jatin Rane <jatinrane23@gmail.com>

--- Frontend/Smart-water-pipeline-inspection-monitoring-system/src/App.jsx
+++ Frontend/Smart-water-pipeline-inspection-monitoring-system/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './App.css'
+import Header from './Components/Common/Header'
+import Footer from './Components/Common/Footer'
+import Home from './Pages/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './Pages/auth/Login'
 
 function App() {
   return (
-    <React.Fragment>
-    </React.Fragment>
+    <BrowserRouter>
+          <Header/>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer></Footer>
+    </BrowserRouter>
+
   )
 }
 
---
+++ Frontend/Smart-water-pipeline-inspection-monitoring-system/src/Pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "../Components/Common/Header"
+function Home(){
+    return (
+        <React.Fragment>
+        </React.Fragment>
+    )
+}
+export default Home;
